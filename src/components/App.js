a5b18bd5b7a543bf63509ecc90a2426fbcb704c3
VishalSaini7272 <sainivishal156@gmail.com>
2025-10-27 13:40:47 +0530

    Fix formatting and import statement in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-
+import React,{useState} from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [count,setCount] = useState(0)
+ const [count,setCount] =  useState(0)
   return (
     <div>
     <p> Button clicked {count} times </p>
