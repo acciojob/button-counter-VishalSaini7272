b86afd0ba8126c92a5f08390dd1c05caf99302ae
VishalSaini7272 <sainivishal156@gmail.com>
2025-10-29 17:13:37 +0530

    Refactor App component with handleClick function
    Refactor App component to improve readability and functionality.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React,{useState} from "react";
+import React, {useState} from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
- const [count,setCount] =  useState(0)
-  return (
+ const [count,setCount ] = useState(0)
+const handleClick = ()
+=> {
+ setCount(count + 1)
+ return (
     <div>
-    <p> Button clicked {count} times </p>
-  <button onClick{()=>setCount (count+ 1)}>click me</button>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+   {/* Do not remove the main div */}
+  <p> Button clicked {count} times </p>
+  <button onClick ={handleClick}> Click me </button> 
+  </div>
+  );
+};
 
-export default App
+export default App;
